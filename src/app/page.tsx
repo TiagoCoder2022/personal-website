772f2886137bb39fc2a5b37909578f6323a7211e
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-03-21 19:08:19 -0300

    feat: add loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
+"use client";
 import About from "@/components/about";
 import CallToAction from "@/components/call-to-action";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Projects from "@/components/projects";
 import Technologies from "@/components/technoligies";
+import LoadingScreen from "@/components/loading-screen";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <main>
-      <Header />    
-      <Hero />      
-      <Technologies />
-      <About />
-      <CallToAction />
-      <Projects />      
-    </main>    
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {isLoaded && (
+        <main>
+          <Header />
+          <Hero />
+          <Technologies />
+          <About />
+          <CallToAction />
+          <Projects />
+        </main>
+      )}
+    </>
   );
 }
