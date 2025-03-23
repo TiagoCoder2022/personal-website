dfc3abc55f887699912a951e5daffb199ce50f54
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-03-22 23:37:47 -0300

    fix: remove no used components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
 "use client";
 import About from "@/components/about";
-import CallToAction from "@/components/call-to-action";
-import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Projects from "@/components/projects";
 import Technologies from "@/components/technoligies";
 import LoadingScreen from "@/components/loading-screen";
 
 import { useState } from "react";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
-
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       {isLoaded && (
         <main>
-          <Header />
+          <Navbar />          
           <Hero />
           <Technologies />
-          <About />
-          <CallToAction />
+          <About />          
           <Projects />
+          <Footer />
         </main>
       )}
     </>
