bf959cf685d46add3791fc6877267cdf4382a051
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-05-17 20:21:55 -0300

    feat: ensure to use projectData for the projects

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -3,7 +3,7 @@ import { FiArrowRight } from "react-icons/fi";
 
 interface ProjectCardProps {
   title: string;
-  description: string;
+  subtitle: string;
   imgSrc: string;
   isHovered: boolean;
   isBlurred: boolean;
@@ -11,19 +11,27 @@ interface ProjectCardProps {
   onMouseLeave: () => void;
 }
 
-const ProjectCard = ({ title, description, imgSrc, isHovered, isBlurred, onMouseEnter, onMouseLeave }: ProjectCardProps) => {
+const ProjectCard = ({
+  title,
+  subtitle,
+  imgSrc,
+  isHovered,
+  isBlurred,
+  onMouseEnter,
+  onMouseLeave,
+}: ProjectCardProps) => {
   return (
-    <div 
+    <div
       className="relative flex-1 group overflow-hidden transition-all duration-300"
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
       {/* Aplicando os efeitos SOMENTE na imagem */}
       <div className="overflow-hidden rounded-xl shadow-md">
-        <Image 
-          src={imgSrc} 
-          height={400}  
-          width={400} 
+        <Image
+          src={imgSrc}
+          height={400}
+          width={400}
           alt="projects"
           className={`w-full h-[300px] md:h-[350px] object-cover transition-all duration-300 ${
             isHovered ? "scale-105" : "scale-100"
@@ -34,14 +42,13 @@ const ProjectCard = ({ title, description, imgSrc, isHovered, isBlurred, onMouse
       <div className="flex flex-row justify-between items-center py-10 gap-6 px-5">
         <div className="text-center md:text-left text-white">
           <h2 className="text-lg font-semibold">{title}</h2>
-          <p className="text-sm text-gray-400">{description}</p>
+          <p className="text-sm text-gray-400">{subtitle}</p>
         </div>
-        
+
         <div className="border-2 border-gray-600 rounded-3xl px-6 py-4 text-white hover:text-black hover:bg-white transition-all duration-300">
-          <FiArrowRight className="text-lg md:text-xl transform group-hover:-rotate-45 transition-transform duration-300 ease-in-out"/>
+          <FiArrowRight className="text-lg md:text-xl transform group-hover:-rotate-45 transition-transform duration-300 ease-in-out" />
         </div>
-        
-      </div>      
+      </div>
     </div>
   );
 };
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,36 +1,35 @@
 "use client"
+
 import { useState } from "react";
+import Link from "next/link";
 import ProjectCard from "./project-card";
+import { projects } from "@/app/data";
 
 const Projects = () => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
-  const projects = [
-    { imgSrc: "/assets/img/project-1.png", title: "Fsw Store", description: "Full-Stack web application" },
-    { imgSrc: "/assets/img/project-2.png", title: "Fsw Foods", description: "Full-Stack web application" },
-    { imgSrc: "/assets/img/project-3.png", title: "Fsw Trips", description: "Full-Stack web application" },
-    { imgSrc: "/assets/img/project-4.png", title: "Finance AI", description: "Full-Stack web application" },
-  ];
 
   return (
     <div className="max-w-screen-xl mx-auto py-12 md:py-36 px-5" id="projects">
       <div className="flex flex-wrap gap-3 items-center mb-16">
         <div className="w-8 h-0.5 bg-white"></div>
-        <h2 className="text-3xl sm:text-4xl text-[#ABABB5] font-bold">Projects</h2>
+        <h2 className="text-3xl sm:text-4xl text-[#ABABB5] font-bold">
+          Projects
+        </h2>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-12">
         {projects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            imgSrc={project.imgSrc}
-            title={project.title}
-            description={project.description}
-            isHovered={hoveredIndex === index}
-            isBlurred={hoveredIndex !== null && hoveredIndex !== index}
-            onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
-          />
+          <Link href={`/projects/${project.slug}`} key={project.id}>
+            <ProjectCard
+              imgSrc={project.imgSrc}
+              title={project.title}
+              subtitle={project.subtitle}
+              isHovered={hoveredIndex === index}
+              isBlurred={hoveredIndex !== null && hoveredIndex !== index}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            />
+          </Link>
         ))}
       </div>
     </div>
