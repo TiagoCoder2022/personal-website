711beac8b317235130e2a99284163d173cfa483b
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-06-05 22:15:13 -0300

    feat: add lucide-react icon library and update contact component layout

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,36 +1,37 @@
-import { FaLocationArrow } from "react-icons/fa";
+// import { FaLocationArrow } from "react-icons/fa";
+import { Navigation } from "lucide-react";
 import MagicButton from "./ui/magic-button";
-import Footer from "./footer";
-
 
 const Contact = () => {
-  
   return (
     <div
-      className="flex flex-col justify-center w-full pt-20 pb-10 max-w-screen-xl mx-auto px-5"
+      className="flex flex-col justify-center rounded-2xl w-full py-10 lg:py-16 max-w-screen-xl mx-auto px-5 bg-bg-800"
       id="contact"
     >
       <div className="flex justify-center flex-col items-center">
-        <h1 className="text-4xl max-w-2xl font-bold text-center">
-          Ready to take <span className="text-[#0070f3]">your</span> digital
-          presence to the next level?
-        </h1>
-        <p className="text-base md:text-lg text-white/80 md:mt-10 my-5 text-center">
-          Let&apos;s work together to build something great. Get in touch with
-          me today.
-        </p>
+        <div className="py-2 px-4 rounded-full bg-bg-600 flex items-center gap-2">
+          <span className="relative flex size-2">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex size-2 rounded-full bg-sky-500"></span>
+          </span>
+
+          <p className="text-sm">Available for work</p>
+        </div>
+        <h2 className="text-3xl lg:text-5xl max-w-md font-semibold text-center my-5 text-text-primary">
+          Let&apos;s create <span className="text-[#0070f3]">your</span> next
+          big idea.
+        </h2>
+
         <a href="mailto:tiagonewalves@hotmail.com">
           <MagicButton
             title="Let's get in touch"
-            icon={<FaLocationArrow />}
+            icon={<Navigation />}
             position="right"
           />
         </a>
       </div>
-
-      <Footer />
     </div>
   );
-}
- 
+};
+
 export default Contact;
